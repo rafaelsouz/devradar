44e27cc2c01583201b9b57c96a8af49083dca469
Rafael Souza <raffaelsouza908@gmail.com>
2020-01-14 22:45:02 -0300

    cadastro do dev pronto

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,19 +1,27 @@
 const { Router } = require('express');
-const axios = require('axios')
+const axios = require('axios')/
+const Dev = require('./models/Dev')
 
 const routes = Router();
 
 
 routes.post('/devs', async ( req, res ) => {
-    const { github_username } = req.body;
+    const { github_username, techs } = req.body;
 
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
     const { name = login, avatar_url, bio } = apiResponse.data;
 
-    console.log(name, avatar_url, bio, github_username );
+    const techsArray = techs.split(',').map( tech => tech.trim() );
     
-    
+    await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray;
+    });
+
     return res.json({ message: 'Hello Omnistack' })
 });
 
