cf4a47e8775c72caeb0d2223d231e669a019de53
Rafael Souza <raffaelsouza908@gmail.com>
2020-01-14 23:55:19 -0300

    adicionado coordenadas no model e listagem de devs pronto

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,28 +1,9 @@
 const { Router } = require('express');
-const axios = require('axios')/
-const Dev = require('./models/Dev')
+const DevController = require('./controllers/DevController')
 
 const routes = Router();
 
-
-routes.post('/devs', async ( req, res ) => {
-    const { github_username, techs } = req.body;
-
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-
-    const { name = login, avatar_url, bio } = apiResponse.data;
-
-    const techsArray = techs.split(',').map( tech => tech.trim() );
-    
-    await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray;
-    });
-
-    return res.json({ message: 'Hello Omnistack' })
-});
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store );
 
 module.exports = routes;
